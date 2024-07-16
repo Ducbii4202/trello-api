48daaa39c1f11b0f1e35014a5a15fcc29da46298
Ducbii4202 <144377938+Ducbii4202@users.noreply.github.com>
2024-07-16 23:24:22 +0700

    Init code

---
+++ .history/src/server_20240716225400.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const hostname = "localhost";
+const port = 8085;
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`hello DucBii, I'm Running server at ${hostname}:${port}`);
+});
